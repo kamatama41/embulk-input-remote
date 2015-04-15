15957cea67fd7f2f6a089042b9265bb13703cce3
kamatama41 <shiketaudonko41@gmail.com>
2015-04-15 10:36:43 +0900

    Add default config test.

--- src/test/java/org/embulk/input/TestRemoteFileInputPlugin.java
+++ src/test/java/org/embulk/input/TestRemoteFileInputPlugin.java
@@ -1,5 +1,34 @@
 package org.embulk.input;
 
+import com.google.common.base.Optional;
+import org.embulk.EmbulkTestRuntime;
+import org.embulk.config.ConfigSource;
+import org.embulk.spi.Exec;
+import org.junit.Rule;
+import org.junit.Test;
+
+import java.util.Collections;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
 public class TestRemoteFileInputPlugin
 {
+    @Rule
+    public EmbulkTestRuntime runtime = new EmbulkTestRuntime();
+
+    @Test
+    public void checkDefaultValues()
+    {
+        ConfigSource config = Exec.newConfigSource();
+
+        RemoteFileInputPlugin.PluginTask task = config.loadConfig(RemoteFileInputPlugin.PluginTask.class);
+		assertThat(task.getHosts(), is(Collections.<String>emptyList()));
+		assertThat(task.getHostsCommand(), is(Optional.<String>absent()));
+		assertThat(task.getHostsSeparator(), is(" "));
+		assertThat(task.getPath(), is(""));
+		assertThat(task.getPathCommand(), is(Optional.<String>absent()));
+		assertThat(task.getAuth(), is(Collections.<String, String>emptyMap()));
+		assertThat(task.getLastTarget(), is(Optional.<RemoteFileInputPlugin.Target>absent()));
+    }
 }
