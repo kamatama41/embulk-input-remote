8455ea01fee28953fae177323511cd311a527cad
Shinichi Ishimura <shiketaudonko41@gmail.com>
2016-12-25 22:22:17 +0900

    Remove unneeded public

--- src/main/java/org/embulk/input/RemoteFileInputPlugin.java
+++ src/main/java/org/embulk/input/RemoteFileInputPlugin.java
@@ -40,48 +40,48 @@ public class RemoteFileInputPlugin
 			extends Task {
 		@Config("hosts")
 		@ConfigDefault("[]")
-		public List<String> getHosts();
+		List<String> getHosts();
 
 		@Config("hosts_command")
 		@ConfigDefault("null")
-		public Optional<String> getHostsCommand();
+		Optional<String> getHostsCommand();
 
 		@Config("hosts_separator")
 		@ConfigDefault("\" \"")
-		public String getHostsSeparator();
+		String getHostsSeparator();
 
 		@Config("port")
 		@ConfigDefault("22")
-		public int getPort();
+		int getPort();
 
 		@Config("path")
 		@ConfigDefault("\"\"")
-		public String getPath();
+		String getPath();
 
 		@Config("path_command")
 		@ConfigDefault("null")
-		public Optional<String> getPathCommand();
+		Optional<String> getPathCommand();
 
 		@Config("auth")
 		@ConfigDefault("{}")
-		public Map<String, String> getAuth();
+		Map<String, String> getAuth();
 
 		@Config("ignore_not_found_hosts")
 		@ConfigDefault("false")
-		public boolean getIgnoreNotFoundHosts();
+		boolean getIgnoreNotFoundHosts();
 
 		@Config("last_target")
 		@ConfigDefault("null")
-		public Optional<Target> getLastTarget();
+		Optional<Target> getLastTarget();
 
-		public void setLastTarget(Optional<Target> lastTarget);
+		void setLastTarget(Optional<Target> lastTarget);
 
-		public List<Target> getTargets();
+		List<Target> getTargets();
 
-		public void setTargets(List<Target> targets);
+		void setTargets(List<Target> targets);
 
 		@ConfigInject
-		public BufferAllocator getBufferAllocator();
+		BufferAllocator getBufferAllocator();
 	}
 
 	private final Logger log = Exec.getLogger(getClass());
