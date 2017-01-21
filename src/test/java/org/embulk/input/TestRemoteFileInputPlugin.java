3b3ed4be03fedec636435acf19a863711a4ffcf4
Shinichi Ishimura <shiketaudonko41@gmail.com>
2017-01-22 03:16:16 +0900

    Add a test for getting from multi hosts

--- src/test/java/org/embulk/input/TestRemoteFileInputPlugin.java
+++ src/test/java/org/embulk/input/TestRemoteFileInputPlugin.java
@@ -2,12 +2,16 @@ package org.embulk.input;
 
 import ch.qos.logback.classic.Level;
 import ch.qos.logback.classic.Logger;
+import com.github.dockerjava.api.DockerClient;
+import com.github.dockerjava.api.model.Container;
+import com.github.dockerjava.core.DockerClientBuilder;
 import org.embulk.config.ConfigSource;
 import org.embulk.spi.InputPlugin;
 import org.embulk.test.MemoryOutputPlugin;
 import org.embulk.test.MyEmbulkTests;
 import org.embulk.test.MyTestingEmbulk;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
@@ -27,6 +31,11 @@ import static org.junit.Assert.assertThat;
 @RunWith(Enclosed.class)
 public class TestRemoteFileInputPlugin {
     public static class OneHost extends TestBase {
+        @BeforeClass
+        public static void startContainers() {
+            startContainer(CONTAINER_ID_HOST1);
+        }
+
         @Test
         public void loadFromRemote() throws Exception
         {
@@ -75,6 +84,36 @@ public class TestRemoteFileInputPlugin {
         }
     }
 
+    public static class MultiHost extends TestBase {
+        @BeforeClass
+        public static void startContainers() {
+            startContainer(CONTAINER_ID_HOST1);
+            startContainer(CONTAINER_ID_HOST2);
+        }
+
+        @Override
+        ConfigSource baseConfig() {
+            ConfigSource baseConfig = super.baseConfig();
+            final ConfigSource multiHost = newConfig()
+                    .set("hosts", Arrays.asList("localhost:10022", "localhost:10023"));
+            return baseConfig.merge(multiHost);
+        }
+
+        @Test
+        public void testDefault() throws Exception
+        {
+            embulk.runInput(baseConfig());
+
+            assertValues(
+                    values(1L, "kamatama41"),
+                    values(2L, "kamatama42"),
+                    values(3L, "kamatama43"),
+                    values(4L, "kamatama44")
+            );
+        }
+
+    }
+
     public abstract static class TestBase {
         @Rule
         public MyTestingEmbulk embulk = (MyTestingEmbulk) MyTestingEmbulk
@@ -113,6 +152,40 @@ public class TestRemoteFileInputPlugin {
 
         List values(Object... values) {
             return Arrays.asList(values);
+        }
+
+        //////////////////////////////
+        // Methods for Docker
+        //////////////////////////////
+
+        static final String CONTAINER_ID_HOST1 = "test_sshd1";
+        static final String CONTAINER_ID_HOST2 = "test_sshd2";
+        private static final DockerClient dockerClient = DockerClientBuilder.getInstance().build();
+
+        static void stopContainer(String containerId) {
+            if (isRunning(containerId)) {
+                dockerClient.stopContainerCmd(containerId).exec();
+            }
+        }
+
+        static void startContainer(String containerId) {
+            if (!isRunning(containerId)) {
+                dockerClient.startContainerCmd(containerId).exec();
+            }
+        }
+
+        private static boolean isRunning(String containerId) {
+            List<Container> containers = dockerClient.listContainersCmd().exec();
+            for (Container container : containers) {
+                for (String name : container.getNames()) {
+                    if (name.contains(containerId)) {
+                        System.out.println("Found " + containerId);
+                        return true;
+                    }
+                }
+            }
+            System.out.println("Not Found " + containerId);
+            return false;
         }
     }
 }
