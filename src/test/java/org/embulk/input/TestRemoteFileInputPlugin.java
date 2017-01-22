da3ca9b8123218a188c77a3f714cf8f06dca9672
Shinichi Ishimura <shiketaudonko41@gmail.com>
2017-01-22 14:31:54 +0900

    Make test flatten

--- src/test/java/org/embulk/input/TestRemoteFileInputPlugin.java
+++ src/test/java/org/embulk/input/TestRemoteFileInputPlugin.java
@@ -17,8 +17,6 @@ import org.junit.BeforeClass;
 import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
-import org.junit.experimental.runners.Enclosed;
-import org.junit.runner.RunWith;
 import org.slf4j.LoggerFactory;
 
 import java.util.Arrays;
@@ -30,223 +28,186 @@ import java.util.Set;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 
-@RunWith(Enclosed.class)
 public class TestRemoteFileInputPlugin {
-    public static class OneHost extends TestBase {
-        @BeforeClass
-        public static void startContainers() {
-            startContainer(CONTAINER_ID_HOST1);
-        }
+    private static final String CONTAINER_ID_HOST1 = "test_sshd1";
+    private static final String CONTAINER_ID_HOST2 = "test_sshd2";
+    private static final DockerClient dockerClient = DockerClientBuilder.getInstance().build();
 
-        @Test
-        public void loadFromRemote() throws Exception
-        {
-            embulk.runInput(baseConfig());
+    @Rule
+    public MyTestingEmbulk embulk = (MyTestingEmbulk) MyTestingEmbulk
+            .builder()
+            .registerPlugin(InputPlugin.class, "remote", RemoteFileInputPlugin.class)
+            .build();
 
-            assertValues(
-                    values(1L, "kamatama41"),
-                    values(2L, "kamatama42")
-            );
-        }
+    @BeforeClass
+    public static void startContainers() {
+        startContainer(CONTAINER_ID_HOST1);
+        startContainer(CONTAINER_ID_HOST2);
+    }
 
-        @Ignore("Cannot pass on TravisCI, although pass on Local Mac OS...")
-        @Test
-        public void loadFromRemoteViaPublicKey() throws Exception
-        {
-            String keyPath = System.getenv("KEY_PATH");
-            if (keyPath == null) {
-                keyPath = "./id_rsa_test";
-            }
-
-            final ConfigSource publicKeyAuth = newConfig().set("auth", newConfig()
-                    .set("type", "public_key")
-                    .set("key_path", keyPath)
-            );
-            embulk.runInput(baseConfig().merge(publicKeyAuth));
-
-            assertValues(
-                    values(1L, "kamatama41"),
-                    values(2L, "kamatama42")
-            );
-        }
-
-        @Test
-        public void testDefaultPort() throws Exception
-        {
-            final ConfigSource defaultPort = newConfig()
-                    .set("hosts", Collections.singletonList("localhost"))
-                    .set("default_port", 10022);
-
-            embulk.runInput(baseConfig().merge(defaultPort));
-
-            assertValues(
-                    values(1L, "kamatama41"),
-                    values(2L, "kamatama42")
-            );
+    @Before
+    public void prepare() {
+        String logLevel = System.getenv("LOG_LEVEL");
+        if (logLevel != null) {
+            // Set log level
+            Logger rootLogger = (Logger) LoggerFactory.getLogger(Logger.ROOT_LOGGER_NAME);
+            rootLogger.setLevel(Level.toLevel(logLevel));
         }
     }
 
-    public static class MultiHost extends TestBase {
-        @BeforeClass
-        public static void startContainers() {
-            startContainer(CONTAINER_ID_HOST1);
-            startContainer(CONTAINER_ID_HOST2);
+    @Test
+    public void loadFromRemote() throws Exception
+    {
+        embulk.runInput(baseConfig());
+        assertValues(values(1L, "kamatama41"));
+    }
+
+    @Ignore("Cannot pass on TravisCI, although pass on Local Mac OS...")
+    @Test
+    public void loadFromRemoteViaPublicKey() throws Exception
+    {
+        String keyPath = System.getenv("KEY_PATH");
+        if (keyPath == null) {
+            keyPath = "./id_rsa_test";
         }
 
-        @Test
-        public void testConfDiff() throws Exception
-        {
-            final ConfigSource host2Config = newConfig()
-                    .set("hosts", Collections.singletonList("localhost:10023"));
-            ConfigSource config = baseConfig().merge(host2Config);
+        final ConfigSource publicKeyAuth = newConfig().set("auth", newConfig()
+                .set("type", "public_key")
+                .set("key_path", keyPath)
+        );
+        embulk.runInput(baseConfig().merge(publicKeyAuth));
 
-            // Run
-            TestingEmbulk.RunResult runResult = embulk.runInput(config);
-            assertValues(
-                    values(3L, "kamatama43"),
-                    values(4L, "kamatama44")
-            );
+        assertValues(values(1L, "kamatama41"));
+    }
 
-            // Re-run with additional host1
-            final ConfigSource multiHost = newConfig()
-                    .set("hosts", Arrays.asList("localhost:10022", "localhost:10023"));
-            config = baseConfig().merge(multiHost);
+    @Test
+    public void testDefaultPort() throws Exception
+    {
+        final ConfigSource defaultPort = newConfig()
+                .set("hosts", Collections.singletonList("localhost"))
+                .set("default_port", 10022);
 
-            embulk.runInput(config, runResult.getConfigDiff());
+        embulk.runInput(baseConfig().merge(defaultPort));
 
-            assertValues(
-                    values(1L, "kamatama41"),
-                    values(2L, "kamatama42")
-            );
+        assertValues(values(1L, "kamatama41"));
+    }
+
+    @Test
+    public void testConfDiff() throws Exception
+    {
+        final ConfigSource host2Config = newConfig()
+                .set("hosts", Collections.singletonList("localhost:10023"));
+        ConfigSource config = baseConfig().merge(host2Config);
+
+        // Run
+        TestingEmbulk.RunResult runResult = embulk.runInput(config);
+        assertValues(values(2L, "kamatama42"));
+
+        // Re-run with additional host1
+        final ConfigSource multiHost = newConfig()
+                .set("hosts", Arrays.asList("localhost:10022", "localhost:10023"));
+        config = baseConfig().merge(multiHost);
+
+        embulk.runInput(config, runResult.getConfigDiff());
+
+        assertValues(values(1L, "kamatama41"));
+    }
+
+    @Test
+    public void testResume() throws Exception
+    {
+        final ConfigSource multiHost = newConfig()
+                .set("hosts", Arrays.asList("localhost:10022", "localhost:10023"));
+        final ConfigSource config = baseConfig().merge(multiHost);
+
+        // Stop host2 temporarily
+        stopContainer(CONTAINER_ID_HOST2);
+
+        // Run (but will fail)
+        EmbulkEmbed.ResumableResult resumableResult = embulk.resume(config);
+
+        assertThat(resumableResult.isSuccessful(), is(false));
+        assertValues(values(1L, "kamatama41"));
+
+        // Start host2 again
+        startContainer(CONTAINER_ID_HOST2);
+
+        // Resume
+        resumableResult = embulk.resume(config, resumableResult.getResumeState());
+
+        assertThat(resumableResult.isSuccessful(), is(true));
+        assertValues(values(2L, "kamatama42"));
+    }
+
+    @Test
+    public void testIgnoreNotFoundHosts() throws Exception
+    {
+        final ConfigSource ignoreNotFoundHosts = newConfig()
+                .set("hosts", Arrays.asList("localhost:10022", "localhost:10023"))
+                .set("ignore_not_found_hosts", true);
+        final ConfigSource config = baseConfig().merge(ignoreNotFoundHosts);
+
+        // Stop host2
+        stopContainer(CONTAINER_ID_HOST2);
+
+        // Run (host2 will be ignored)
+        EmbulkEmbed.ResumableResult resumableResult = embulk.resume(config);
+
+        assertThat(resumableResult.isSuccessful(), is(true));
+        assertValues(values(1L, "kamatama41"));
+    }
+
+    private ConfigSource baseConfig() {
+        return MyEmbulkTests.configFromResource("yaml/base.yml");
+    }
+
+    private ConfigSource newConfig() {
+        return embulk.newConfig();
+    }
+
+    private void assertValues(List... valuesList) {
+        Set<List> actual = new HashSet<>();
+        for (MemoryOutputPlugin.Record record : MemoryOutputPlugin.getRecords()) {
+            actual.add(record.getValues());
         }
 
-        @Test
-        public void testResume() throws Exception
-        {
-            final ConfigSource multiHost = newConfig()
-                    .set("hosts", Arrays.asList("localhost:10022", "localhost:10023"));
-            final ConfigSource config = baseConfig().merge(multiHost);
+        Set<List> expected = new HashSet<>();
+        Collections.addAll(expected, valuesList);
 
-            // Stop host2 temporarily
-            stopContainer(CONTAINER_ID_HOST2);
+        assertThat(actual, is(expected));
+    }
 
-            // Run (but will fail)
-            EmbulkEmbed.ResumableResult resumableResult = embulk.resume(config);
+    private List values(Object... values) {
+        return Arrays.asList(values);
+    }
 
-            assertThat(resumableResult.isSuccessful(), is(false));
-            assertValues(
-                    values(1L, "kamatama41"),
-                    values(2L, "kamatama42")
-            );
+    //////////////////////////////
+    // Methods for Docker
+    //////////////////////////////
 
-            // Start host2 again
-            startContainer(CONTAINER_ID_HOST2);
-
-            // Resume
-            resumableResult = embulk.resume(config, resumableResult.getResumeState());
-
-            assertThat(resumableResult.isSuccessful(), is(true));
-            assertValues(
-                    values(3L, "kamatama43"),
-                    values(4L, "kamatama44")
-            );
-        }
-
-        @Test
-        public void testIgnoreNotFoundHosts() throws Exception
-        {
-            final ConfigSource ignoreNotFoundHosts = newConfig()
-                    .set("hosts", Arrays.asList("localhost:10022", "localhost:10023"))
-                    .set("ignore_not_found_hosts", true);
-            final ConfigSource config = baseConfig().merge(ignoreNotFoundHosts);
-
-            // Stop host2
-            stopContainer(CONTAINER_ID_HOST2);
-
-            // Run (host2 will be ignored)
-            EmbulkEmbed.ResumableResult resumableResult = embulk.resume(config);
-
-            assertThat(resumableResult.isSuccessful(), is(true));
-            assertValues(
-                    values(1L, "kamatama41"),
-                    values(2L, "kamatama42")
-            );
+    private static void stopContainer(String containerId) {
+        if (isRunning(containerId)) {
+            dockerClient.stopContainerCmd(containerId).exec();
         }
     }
 
-    public abstract static class TestBase {
-        @Rule
-        public MyTestingEmbulk embulk = (MyTestingEmbulk) MyTestingEmbulk
-                .builder()
-                .registerPlugin(InputPlugin.class, "remote", RemoteFileInputPlugin.class)
-                .build();
-
-        @Before
-        public void prepare() {
-            String logLevel = System.getenv("LOG_LEVEL");
-            if (logLevel != null) {
-                // Set log level
-                Logger rootLogger = (Logger) LoggerFactory.getLogger(Logger.ROOT_LOGGER_NAME);
-                rootLogger.setLevel(Level.toLevel(logLevel));
-            }
+    static void startContainer(String containerId) {
+        if (!isRunning(containerId)) {
+            dockerClient.startContainerCmd(containerId).exec();
         }
+    }
 
-        ConfigSource baseConfig() {
-            return MyEmbulkTests.configFromResource("yaml/base.yml");
-        }
-
-        ConfigSource newConfig() {
-            return embulk.newConfig();
-        }
-
-        void assertValues(List... valuesList) {
-            Set<List> actual = new HashSet<>();
-            for (MemoryOutputPlugin.Record record : MemoryOutputPlugin.getRecords()) {
-                actual.add(record.getValues());
-            }
-
-            Set<List> expected = new HashSet<>();
-            Collections.addAll(expected, valuesList);
-
-            assertThat(actual, is(expected));
-        }
-
-        List values(Object... values) {
-            return Arrays.asList(values);
-        }
-
-        //////////////////////////////
-        // Methods for Docker
-        //////////////////////////////
-
-        static final String CONTAINER_ID_HOST1 = "test_sshd1";
-        static final String CONTAINER_ID_HOST2 = "test_sshd2";
-        private static final DockerClient dockerClient = DockerClientBuilder.getInstance().build();
-
-        static void stopContainer(String containerId) {
-            if (isRunning(containerId)) {
-                dockerClient.stopContainerCmd(containerId).exec();
-            }
-        }
-
-        static void startContainer(String containerId) {
-            if (!isRunning(containerId)) {
-                dockerClient.startContainerCmd(containerId).exec();
-            }
-        }
-
-        private static boolean isRunning(String containerId) {
-            List<Container> containers = dockerClient.listContainersCmd().exec();
-            for (Container container : containers) {
-                for (String name : container.getNames()) {
-                    if (name.contains(containerId)) {
-                        System.out.println("Found " + containerId);
-                        return true;
-                    }
+    private static boolean isRunning(String containerId) {
+        List<Container> containers = dockerClient.listContainersCmd().exec();
+        for (Container container : containers) {
+            for (String name : container.getNames()) {
+                if (name.contains(containerId)) {
+                    System.out.println("Found " + containerId);
+                    return true;
                 }
             }
-            System.out.println("Not Found " + containerId);
-            return false;
         }
+        System.out.println("Not Found " + containerId);
+        return false;
     }
 }
